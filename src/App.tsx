7766337e2f0cff11e430f99a20f06a1906660909
Vinícius Catafesta Francisco <vinicius.francisco@spartalabs.com.br>
2022-01-03 13:30:01 -0300

    configured custom fonts and icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
-import Icon from 'react-native-vector-icons/Feather';
 import { NavigationContainer } from '@react-navigation/native';
 import { ThemeProvider } from 'styled-components/native';
 import theme from './assets/theme';
 import AppNavigation from './navigation/AppNavigation';
 
 const App: React.FC = () => {
-  Icon.loadFont();
-
   return (
     <NavigationContainer>
       <ThemeProvider theme={theme}>
