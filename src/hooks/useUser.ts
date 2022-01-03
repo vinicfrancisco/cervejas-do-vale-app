3d39e41be14be77c9cea16a4b59bc5085b69bacf
Vinícius Catafesta Francisco <vinicius.francisco@spartalabs.com.br>
2022-01-03 15:58:59 -0300

    fixed use user hook name

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,9 +1,9 @@
 import { useContextSelector } from 'use-context-selector';
 import { AuthContext, AuthContextData } from '~/contexts/AuthContext';
 
-type UseUserData = Pick<AuthContextData, 'user'>;
+type UseUser = Pick<AuthContextData, 'user'>;
 
-export default function useUserData(): UseUserData {
+export default function useUser(): UseUser {
   const user = useContextSelector(AuthContext, state => state.user);
 
   return {
