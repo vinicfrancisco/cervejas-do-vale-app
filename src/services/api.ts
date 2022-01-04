948c02ff8f19c9ddf04e278ca8a75ab4e57de579
Vinícius Catafesta Francisco <vinicius.francisco@spartalabs.com.br>
2022-01-04 16:30:33 -0300

    added platform host verification - development

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,15 @@
+import { Platform } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios, { AxiosRequestConfig } from 'axios';
 import { USER_TOKEN } from '~/util/consts';
 
+const host = Platform.select({
+  android: '192.168.1.9',
+  ios: 'localhost',
+});
+
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: `http://${host}:3333`,
 });
 
 api.interceptors.request.use(async (config: AxiosRequestConfig) => {
