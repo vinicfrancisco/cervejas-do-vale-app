83a731e54a521f953999879b54ab456554b846c3
Vinícius Catafesta Francisco <vinicius.francisco@spartalabs.com.br>
2022-01-04 09:25:41 -0300

    configured splash screen and icon on android

--- android/app/src/main/java/com/cervejasdovale/MainActivity.java
+++ android/app/src/main/java/com/cervejasdovale/MainActivity.java
@@ -3,9 +3,12 @@ package com.cervejasdovale;
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 
+import org.devio.rn.splashscreen.SplashScreen;
+
 public class MainActivity extends ReactActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this);
     super.onCreate(null);
   }
 
